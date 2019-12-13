39b982779fe91639d8a14aacc6ba9ecb23ced2ac
GLATT Nicolas <Nicolas.Glatt@bordeaux-inp.fr>
2019-12-13 15:19:38 +0100

    la chance

--- mib.m
+++ mib.m
@@ -2,5 +2,5 @@ function[Image, Mask, Box] = mib(Image)
 
 [w,h] = size(Image);
 Mask = ones(w,h);
-Box = [1 1; w h];
+Box = [1 1; 1 h; w h; w 1];
 end
--- proj2.m
+++ proj2.m
@@ -7,22 +7,27 @@
 
 clear ; clc ; close all ;
 
-IMG1 = double(imread("images/mos1.bmp"));
-IMG2 = double(imread("images/mos20.bmp"));
+IMG1 = double(imread("Dragon02.png"))/256;
+IMG2 = double(imread("Dragon01.png"))/256;
+
 [h1, w1] = size(IMG1);
 [h2, w2] = size(IMG2);
+
+IMG1 = rgb2gray(IMG1);
+IMG2 = rgb2gray(IMG2);
 
 [IMG1, Mask1, box1] = mib(IMG1);
 [IMG2, Mask2, box2] = mib(IMG2);
 
-figure(1), imagesc(IMG1), colormap(gray);
-[XA, YA] = ginput(4);
-close(1);
+% figure(1), imagesc(IMG1), colormap(gray);
+% [XA, YA] = ginput(4);
+% close(1);
+% 
+% figure(2), imagesc(IMG2), colormap(gray);
+% [XB, YB] = ginput(4);
+% close(2);
 
-figure(2), imagesc(IMG2), colormap(gray);
-[XB, YB] = ginput(4);
-close(2);
-
+load("points.mat");
 
 %% PROJECTION
 
@@ -31,7 +36,7 @@ close(2);
 H1 = homographie(XA, YA, XB, YB);
 H2 = homographie(XB, YB, XA, YA);
 
-[IMG11, Mask11, box11] = mib2(IMG1, Mask1, box1, H1);
-% 
+[IMG11, Mask11, box11] = mib2(IMG1, Mask1, box1, H2, H1);
+
 % [IMG, Mask, box] = fusion(IMG11, Mask11, box11, IMG2, Mask2, box2);
 
