a16df372d715580de36e87493db3dcaf838a297a
BAGARRE Alexis <Alexis.Bagarre@bordeaux-inp.fr>
2019-12-15 14:52:19 +0100

    BE mib2 correct

--- mib2.m
+++ mib2.m
@@ -1,25 +1,11 @@
 function[Image_final, Mask_final, Box_final] = mib2(Image, Mask, Box, Homographie, Homographie_inverse)
 
 
-[box_1_x, box_1_y] = transformation(Homographie, Box(1,1), Box(1,2));
-[box_2_x, box_2_y] = transformation(Homographie, Box(2,1), Box(2,2));
-[box_3_x, box_3_y] = transformation(Homographie, Box(3,1), Box(3,2));
-[box_4_x, box_4_y] = transformation(Homographie, Box(4,1), Box(4,2));
+[box_1_x, box_1_y] = transformation(inv(Homographie_inverse), Box(1,1), Box(1,2));
+[box_2_x, box_2_y] = transformation(inv(Homographie_inverse), Box(2,1), Box(2,2));
+[box_3_x, box_3_y] = transformation(inv(Homographie_inverse), Box(3,1), Box(3,2));
+[box_4_x, box_4_y] = transformation(inv(Homographie_inverse), Box(4,1), Box(4,2));
 Box_final = [floor(box_1_x) floor(box_1_y); floor(box_2_x), floor(box_2_y); floor(box_3_x), floor(box_3_y); floor(box_4_x), floor(box_4_y)];
-
-
-% [w,h] = size(Image);
-% 
-% Test1 = ones(w,h);
-% Test2 = ones(w,h);
-% 
-% for i = 1:w
-%     for j = 1:h
-%         [x, y] = transformation(Homographie, i, j);
-%         Test1(i,j) = x;
-%         Test2(i,j) = y;
-%     end
-% end
 
 Max_x = max([floor(box_1_x) floor(box_2_x) floor(box_3_x) floor(box_4_x)]);
 Min_x = min([floor(box_1_x) floor(box_2_x) floor(box_3_x) floor(box_4_x)]);
@@ -32,7 +18,6 @@ Mask_final = Image_final;
 
 [W,H] = size(Image_final);
 
-
 for i = 1:W+1
     for j = 1:H+1
         [x, y] = transformation(Homographie_inverse, i+Min_x-1, j+Min_y-1);
@@ -43,11 +28,7 @@ for i = 1:W+1
     end
 end
 
-% for i = 1:w
-%     for j = 1:h
-%         Image1(floor(Test1(i,j)-Min_x+1),floor(Test2(i,j)-Min_y+1)) = Image(i,j);
-%         Mask1(Test1(i,j)-Min_x+1,Test2(i,j)-Min_y+1) = Mask(i,j);
-%     end
-% end
+% Image_final = Image_final(~all(Image_final == 0, 2),:);
+% Image_final = Image_final(:,~all(Image_final == 0, 1));
 
 end
